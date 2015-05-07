e2024d66e8eedb21f2651270620a1a41c7527b45
Eugenio Mendoza Robaina <eu.mendoza@gmail.com>
2015-05-07 12:14:14 +0200

    Desde un .json cargamos el dataGrid de miembros

--- app/scripts/controllers/miembros.js
+++ app/scripts/controllers/miembros.js
@@ -8,10 +8,18 @@
  * Controller of the protoYeomanApp
  */
 angular.module('protoYeomanApp')
-  .controller('MiembrosCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MiembrosCtrl', function ($scope,$http) {
+    $http.get('http://localhost:9000/miembros.json').success(function(data){
+    	$scope.miembros = data;
+    });
+    $scope.gridOptions = {
+    	data: 'miembros',
+    	columnDefs: [
+    		{field:'no', displayName:'Nº.'},
+    		{field:'nombre', displayName:'Nombre'},
+    		{field:'fidelidad', displayName:'Puntos Fidelidad'},
+    		{field:'fechaUnion', displayName:'Fecha de Unión'},
+    		{field:'tipoMiembro', displayName:'Tipo de Miembro'}
+    	]
+    };
   });
